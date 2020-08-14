b331886a9fc965ba356a38d2a4e3dc71396f5c0e
sjw <941076242@qq.com>
2020-08-14 15:32:07 +0800

    修改babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset", ["es2015", { "modules": false }]],
+  presets: ["@vue/cli-plugin-babel/preset", ["@babel/preset-env", { "modules": false }]],
   "plugins": [
     [
       "component",
