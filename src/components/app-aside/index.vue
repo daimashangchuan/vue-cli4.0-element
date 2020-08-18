1971388653b0ffbc06fef6d919ecbb797f6de299
sjw <941076242@qq.com>
2020-08-18 09:25:08 +0800

    对侧边栏进行二次封装

--- src/components/app-aside/index.vue
+++ src/components/app-aside/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="appAside" :style="{ height: appHeight }">
     <el-menu :default-active="defaultActive" @open="handleOpen" @close="handleClose" :unique-opened="true">
-      <div v-for="(menu,index) in menuData" :key="index">
-        <el-menu-item v-if="!menu.childs" :index="menu.value" :disabled='menu.disabled'>
-          <i :class="el-icon-document"></i>
-          <span slot="title">{{ menu.title }}</span>
-        </el-menu-item>
+      <div v-for="(menuOne,index) in menuData" :key="index">
+        <el-submenu :index="menuOne.path">
+          <template slot="title">
+            <i :class="menuOne.icon"></i>
+            <span slot="title">{{ menuOne.title }}</span>
+          </template>
+          <el-menu-item @click="goRouterPath(menuOne.path)" v-if="!menuOne.childs" :index="menuOne.path" :disabled='menuOne.disabled'>
+            <i :class="menuOne.icon"></i>
+            <span slot="title">{{ menuOne.title }}</span>
+          </el-menu-item>
+        </el-submenu>
+        <!-- 一级路由 -->
+        <!-- <el-menu-item @click="goRouterPath(menuOne.path)" v-if="!menuOne.childs" :index="menuOne.path" :disabled='menuOne.disabled'>
+          <i :class="menuOne.icon"></i>
+          <span slot="title">{{ menuOne.title }}</span>
+        </el-menu-item> -->
+        <!-- 二级路由 -->
+        <!-- <template v-if="menuOne.childs">
+          <el-submenu v-for="(menuTwo,i) in menuOne" :key="i" :index="menuTwo.path">
+            <template slot="title">
+              <i :class="menuOne.icon"></i>
+              <span slot="title">{{ menuOne.title }}</span>
+            </template>
+            <el-menu-item @click="goRouterPath(menuTwo.path)" v-if="!menuTwo.childs" :index="menuTwo.path" :disabled='menuTwo.disabled'>
+              <i :class="menuTwo.icon"></i>
+              <span slot="title">{{ menuTwo.title }}</span>
+            </el-menu-item>
+          </el-submenu>
+        </template> -->
       </div>
     </el-menu>
   </div>
@@ -37,8 +61,13 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
+    // 设置侧边栏的 height
     setAsideHeight() {
       this.appHeight = this.clientHeight - 120 + "px";
+    },
+    // 跳转路由
+    goRouterPath(path) {
+      this.$router.push({ path });
     }
   },
 };
--- src/data/menu.js
+++ src/data/menu.js
@@ -7,32 +7,32 @@ const menuData = [
     name: "message",
     icon: "el-icon-message",
     title: "权限管理",
-    value: "",
+    path: "",
     disabled: false,
     // 二级
     childs: [{
       name: "auth",
       icon: "el-icon-loading",
       title: "权限管理",
-      value: "/auth",
+      path: "/auth",
       disabled: false,
     }, {
       name: "role",
       icon: "el-icon-bell",
       title: "角色管理",
-      value: "/role",
+      path: "/role",
       disabled: false,
     }, {
       name: "menu",
       icon: "el-icon-edit",
       title: "菜单管理",
-      value: "/menu",
+      path: "/menu",
       disabled: false,
     }, {
       name: "group",
       icon: "el-icon-mobile-phone\r\n",
       title: "分组管理",
-      value: "/group",
+      path: "/group",
       disabled: false,
     }],
   }, 
@@ -41,27 +41,27 @@ const menuData = [
     name: "user",
     icon: "el-icon-news",
     title: "帐号管理",
-    value: "",
+    path: "",
     disabled: false,
     // 二级
     childs: [{
       name: "account",
       icon: "el-icon-phone-outline\r\n",
       title: "帐号管理",
-      value: "/account",
+      path: "/account",
       disabled: false,
     }, {
       name: "passManage",
       icon: "el-icon-service\r\n",
       title: "密码管理",
-      value: "",
+      path: "",
       disabled: false,
       // 三级
       childs: [{
         name: "integralManage",
         icon: "el-icon-picture",
         title: "积分管理",
-        value: "/integral",
+        path: "/integral",
         disabled: false,
       }]
     }],
@@ -71,22 +71,22 @@ const menuData = [
     name: "contentManage",
     icon: "el-icon-rank",
     title: "分类管理",
-    value: "",
+    path: "",
     disabled: false,
     // 二级
     childs: [{
       name: "classifyManage",
       icon: "el-icon-printer",
       title: "分类管理",
-      value: "/classify",
+      path: "/classify",
       disabled: false,
     }],
   },{
     // 一级
     name: "document",
     icon: "el-icon-document",
-    title: "分类管理",
-    value: "/document",
+    title: "积分管理",
+    path: "/document",
     disabled: false,
   }
 ];
